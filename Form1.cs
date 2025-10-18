c2b39f872b04110056b91ed3d05a5087c1ab8b27
Valery Novokreshchenova <lera89139119747@gmail.com>
2025-10-19 01:21:15 +0700

    Код  по ккнопкам

---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ПО
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button8_MouseEnter(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            btn.FlatAppearance.BorderColor = Color.DarkGray; // Тёмная граница
+            btn.BackColor = Color.FromArgb(40, 40, 40);
+            btn.ForeColor = Color.White;
+        }
+
+        private void button8_MouseLeave(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            btn.FlatAppearance.BorderColor = SystemColors.Control;
+            btn.BackColor = SystemColors.Control;
+            btn.ForeColor = SystemColors.ControlText;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
